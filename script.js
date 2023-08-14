28ae161f8554e08d774bf5ce3c43adca3d20fec1
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-13 20:31:33 -0500

    book constructor made > added function to push books to library array added user in put form > ui updated

--- script.js
+++ script.js
@@ -1,120 +1,61 @@
-const container = document.querySelector('.container');
+//new book constructor
+function Book(title, author, pages) {
+  this.title = title;
+  this.authour = author;
+  this.pages = pages;
+  //method to log book details
+  this.info = () => {
+    return `${title} by: ${author}, ${pageCount} pages, has been read? ${isRead}`;
+  }
+}
 
-document.querySelector('.cancel').addEventListener('click', closeForm)
-document.getElementById('form').addEventListener('submit', addBook);
-document.getElementById('resetBooks').addEventListener('click', resetCards);
-container.addEventListener("click", changeBook);
-
+//book storing array
 let myLibrary = [];
 
-function getBookFromInput(book) {
-  let title = document.getElementById('formTitle').value;
-  let author = document.getElementById('formAuthor').value;
-  let pages = document.getElementById('formPages').value;
-  let isRead = document.querySelector('input[name="formRead"]:checked').value;
-  return new Book(title, author, pages, isRead);
+function addBookToLibrary() {
+  newTitle = document.getElementById('form-title').value;
+  newAuthor = document.getElementById('form-author').value;
+  pages = document.getElementById('form-pages').value;
+
+  return new Book(newTitle, newAuthor, pages);
 }
 
+//push new book to library on click
+document.getElementById('push').addEventListener('click', () => {
+  myLibrary.push(addBookToLibrary());
+  console.log(myLibrary);
+});
 
-function addBook(book) {
-  let newBook = getBookFromInput(book);
-  addBookToLibrary(newBook);
-  displayBooks()
-  closeForm();
-  book.preventDefault();
-  document.getElementById("form").reset();
-}
+//has the book been read?
 
-function resetCards() {
-  while(container.firstChild) {
-    container.removeChild(container.lastChild);
-  }
-  myLibrary = [];
-}
+//FIX THIS!!!
+function isRead(){
+  let isChecked = document.querySelector('.read');
+  let readDisplay = document.querySelector('.read-display');
+  let toggle = false;
 
-function changeBook(e) {
-  if (e.target.classList.contains("removeButton")) {
-    let index = e.target.getAttribute('data-index');
-    let cardToRemove = document.querySelector(`[data-index="${index}"]`);
-    container.removeChild(cardToRemove);
-    myLibrary.splice(index,1);
-  }
-  else if (e.target.classList.contains("readStatusBtn")) {
-    let index = e.target.getAttribute('data-index');
-    let cardToChange = document.querySelector(`[data-index="${index}"]`);
-    let readStatus = cardToChange.querySelector('.isReadStatus');
-    if (readStatus.innerHTML === "Read") {
-      readStatus.innerHTML = "Not Read";
-      myLibrary[index].isRead = "Not Read";
-    } else {
-      readStatus.innerHTML = "Read";
-      myLibrary[index].isRead = "Read";
-
+  isChecked.addEventListener('click', ()=>{
+    if(toggle === false){
+      readDisplay.textContent = 'I\'ve read it';
+      toggle = true;
     }
 
-  }
+    isChecked.addEventListener('click', ()=>{
+      if(toggle === true){
+        readDisplay.textContent = 'No, not yet';
+        toggle = false;
+      }
+     });
+  });
 }
 
-class Book {
-    constructor(
-      title = "Unknown",
-      author = "Unknown",
-      pages = "0",
-      isRead = "Not Read",
-    ) {
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-      this.isRead = isRead;
-    }
-}
+isRead();
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-}
 
-function displayBooks() {
-    makeCard(myLibrary[myLibrary.length -1]);
-}
 
-function makeCard(bookToAdd)
-{
-    let card = document.createElement('div');
-    let title = document.createElement('h1');
-    let author = document.createElement('h2');
-    let pages = document.createElement('p');
-    let isRead = document.createElement('p');
-    let readStatusBtn = document.createElement('BUTTON');
-    let removeButton = document.createElement('BUTTON');
-    
-    isRead.className = 'isReadStatus';
-    readStatusBtn.className = 'readStatusBtn';
-    readStatusBtn.innerText = 'Change Read';
-    readStatusBtn.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
-    removeButton.className = 'removeButton';
-    removeButton.innerText = 'Remove Book';
-    removeButton.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
-    card.classList.add('card');
 
-    title.textContent = bookToAdd.title;
-    author.textContent = bookToAdd.author;
-    pages.textContent = `${bookToAdd.pages} pages`;
-    isRead.textContent = bookToAdd.isRead;
 
-    card.append(title);
-    card.append(author);
-    card.append(pages);
-    card.append(isRead);
-    card.append(removeButton);
-    card.append(readStatusBtn);
-    card.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
-    container.append(card);
-}
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+
+
