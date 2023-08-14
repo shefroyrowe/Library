a90043872bbda73973c18d062511d30349b1711d
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-14 14:33:14 -0500

    books displayed on cards via js > ui updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //new book constructor
 function Book(title, author, pages, isRead) {
   this.title = title;
-  this.authour = author;
+  this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  
+
   /*method to log book details
   this.info = () => {
     return `${title} by: ${author}, ${pageCount} pages, has been read? ${isRead}`;
@@ -27,5 +27,35 @@ function addBookToLibrary() {
 //push new book to library on click
 document.getElementById('push').addEventListener('click', () => {
   myLibrary.push(addBookToLibrary());
+
   console.log(myLibrary);
+
+  displayBook();
 });
+
+//display books function 
+function displayBook(){
+  let card = document.createElement('div');
+  card.classList.add('card');
+  
+  let bookContainer = document.querySelector('.book-cards');
+  bookContainer.appendChild(card);
+  
+
+  let bookTitle = document.createElement('p');
+  let bookAuthor = document.createElement('p');
+  let bookPages = document.createElement('p');
+  let bookReadStatus = document.createElement('p');
+
+  myLibrary.forEach(book => {
+    bookTitle.textContent = `Title: ${book.title}`;
+    bookAuthor.textContent = `Author: ${book.author}`;
+    bookPages.textContent = `${book.pages} pages`;
+    bookReadStatus.textContent = `Book is read: [${book.isRead}]`;
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookReadStatus);
+  });
+}
